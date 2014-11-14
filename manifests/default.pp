b1d5e390964961c741d67ce475332d7950957022
Brad Egler <begler@gmail.com>
2014-11-14 11:04:31 -0500

    puppet manifest changes

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,29 +1,38 @@
-Exec {
-    path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+node 'ubuntu/trusty64' {
+
+    Class['os-prep'] -> Class['docker-images']
 }
 
-# Install some default packages
-$default_packages = [ 'build-essential', 'curl', 'git', 'subversion', 'software-properties-common', 'python-software-properties',]
-package { $default_packages :
-    ensure => present,
+class os-prep {
+
+    # Install some default packages
+    $default_packages = [ 'build-essential', 'curl', 'git', 'subversion', 'software-properties-common', 'python-software-properties',]
+    package { $default_packages :
+        ensure => present,
+    }
+
+    class{git:
+        svn => true,
+        gui => true,
+    }
+
+    git::repo{'dotfiles':
+        path   => '/opt/repos/dotfiles',
+        source => 'https://github.com/bradegler/dotfiles.git'
+    }
+
 }
 
-include 'docker'
+class docker-images {
 
-docker::image { 'bradegler/java7': }
-docker::image { 'bradegler/java8': }
-docker::image { 'bradegler/scala': }
-docker::image { 'bradegler/node': }
-docker::image { 'bradegler/go': }
-docker::image { 'bradegler/redis': }
-docker::image { 'bradegler/nginx': }
+    include 'docker'
 
-class{git:
-    svn => true,
-    gui => true,
-}
+    docker::image { 'bradegler/java7': }
+    docker::image { 'bradegler/java8': }
+    docker::image { 'bradegler/scala': }
+    docker::image { 'bradegler/node': }
+    docker::image { 'bradegler/go': }
+    docker::image { 'bradegler/redis': }
+    docker::image { 'bradegler/nginx': }
 
-git::repo{'dotfiles':
-    path   => '/opt/repos/dotfiles',
-    source => 'https://github.com/bradegler/dotfiles.git'
 }
