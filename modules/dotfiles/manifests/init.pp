d4be01d20b0592396e91078c9849ed3900875105
Brad Egler <begler@gmail.com>
2014-11-21 09:20:42 -0500

    dotfiles module updates

--- modules/dotfiles/manifests/init.pp
+++ modules/dotfiles/manifests/init.pp
@@ -1,19 +1,39 @@
 class dotfiles {
 
+    file {'/home/vagrant':
+      ensure => directory,
+      mode   => 644,
+    }
+
+    file { '/home/vagrant/.ssh':
+      ensure  => directory,
+      mode    => 600,
+      require => File['/home/vagrant'],
+    }
+
+    file { '/home/vagrant/.ssh/config':
+      ensure  => file,
+      content => "Host github.com\nHostName ssh.github.com\nPort 443",
+      require => File['/home/vagrant/.ssh'],
+    }
+
     vcsrepo { '/opt/repos/dotfiles':
       ensure   => present,
       provider => git,
       source   => "https://github.com/bradegler/dotfiles.git",
+      require  => File['/home/vagrant/.ssh/config'],
     }
 
-    # file { "/home/vagrant/.dotfiles":
-    #     ensure => 'link',
-    #     target => '/opt/repos/dotfiles',
-    #     require => Vcsrepo[ "/opt/repos/dotfiles"],
-    # }
+    file { "/home/vagrant/.dotfiles":
+      ensure => 'link',
+      target => '/opt/repos/dotfiles',
+      require => Vcsrepo[ "/opt/repos/dotfiles"],
+    }
 
-    # exec { "link dotfiles":
-    #     command => "/opt/repos/dotfiles/setup.sh",
-    #     require => File["/home/vagrant/.dotfiles"],
-    # }
+    exec { "link dotfiles":
+      cwd     => "/home/vagrant",
+      command => ".dotfiles/setup.sh /home/vagrant",
+      creates => "/home/vagrant/.shellaliases",
+      require => File["/home/vagrant/.dotfiles"],
+    }
 }
